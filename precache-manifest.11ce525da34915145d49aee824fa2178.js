ee98f992103f273930e188ddce87546d2daf728f
naveen <gnkr0605@gmail.com>
2019-07-11 14:20:23 +0530

    Updates

---
+++ precache-manifest.11ce525da34915145d49aee824fa2178.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "93213f7d31a10ba021c6",
+    "url": "/contactmanager/static/js/main.bc5dce2a.chunk.js"
+  },
+  {
+    "revision": "1483aeeb8994cda0090d",
+    "url": "/contactmanager/static/js/2.0e8144e5.chunk.js"
+  },
+  {
+    "revision": "93213f7d31a10ba021c6",
+    "url": "/contactmanager/static/css/main.4aac5ecc.chunk.css"
+  },
+  {
+    "revision": "1483aeeb8994cda0090d",
+    "url": "/contactmanager/static/css/2.8e6b235a.chunk.css"
+  },
+  {
+    "revision": "3e302ea51d8a7acc5880cd71616b8963",
+    "url": "/contactmanager/index.html"
+  }
+];
